38a3f7762214e798603a0a3572046ede49a6bcfc
Chirag Dhuria <166009631+chiragdhuria@users.noreply.github.com>
2024-06-23 15:15:28 +0530

    Add files via upload

---
+++ server/routes/posts.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+import { getPosts } from '../controllers/posts.js';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('THIS WORKS!'); 
+});
+
+export default router;
